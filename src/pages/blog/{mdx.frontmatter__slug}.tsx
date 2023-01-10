58893dd125147db945db1996f64956e60117441f
jihye0526 <samtafe97@naver.com>
2023-01-10 22:11:48 +0900

    10.13 Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,33 @@
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/layout";
+import Seo from "../../components/Seo";
 
-export default function BlogPost(){
-    return (
-        <Layout title="Blog Post">
-            
-        </Layout>
-    )
+interface IBlogPostProps {
+    data: Queries.PostDetailQuery,
+    children: any,
 }
+
+export default function BlogPost({data, children} : IBlogPostProps){
+    return (
+        <Layout title="">
+            <div>{children}</div>
+        </Layout>
+    );
+}
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({data} : IBlogPostProps) => <Seo title={data.mdx?.frontmatter?.title!}></Seo>;
