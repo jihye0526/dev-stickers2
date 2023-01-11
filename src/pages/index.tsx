f154bc6eb860245cee5279830f263fc292aa0012
jihye0526 <samtafe97@naver.com>
2023-01-11 21:30:26 +0900

    10.14 StaticImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import * as React from "react"
 import { PageProps } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/Seo";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function IndexPage() {
   return (
     <Layout title="Welcome to DevStickers 👏">
-      <div>
-      </div>
+      <StaticImage 
+        src="https://images.unsplash.com/photo-1589384267710-7a170981ca78?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" 
+        alt="Stickers on the Notebook computer"
+        height={500}></StaticImage>
     </Layout>
   )
 }
