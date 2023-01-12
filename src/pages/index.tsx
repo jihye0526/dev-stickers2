4bce8f6aa8e6dcbbf642827f9ca5d444a7aab824
jihye0526 <samtafe97@naver.com>
2023-01-12 21:18:33 +0900

    10.16 Contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import * as React from "react"
-import { PageProps } from "gatsby"
+import { graphql, PageProps } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/Seo";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-export default function IndexPage() {
+export default function IndexPage({data} : PageProps<Queries.StickersQuery>) {
   return (
     <Layout title="Welcome to DevStickers 👏">
-      <StaticImage 
-        src="https://images.unsplash.com/photo-1589384267710-7a170981ca78?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" 
-        alt="Stickers on the Notebook computer"
-        height={500}></StaticImage>
+      {data.allContentfulStickerPack.nodes.map(sticker => 
+        <article>
+          <GatsbyImage image={getImage(sticker.preview?.gatsbyImageData!)!} alt={sticker.name!} />
+          <h2>{sticker.name}</h2>
+          <h4>${sticker.price}</h4>
+        </article>
+      )}
     </Layout>
   )
 }
+
+export const query  = graphql`
+  query Stickers {
+    allContentfulStickerPack {
+      nodes {
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 500)
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Home"></Seo>;
