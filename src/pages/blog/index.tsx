ced764dc37fc9a182117eb697d5449fd30c46904
jihye0526 <samtafe97@naver.com>
2023-01-13 20:18:44 +0900

    10.18 PicoCSS

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -6,14 +6,13 @@ import Seo from '../../components/Seo';
 export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>){
     return (
         <Layout title="Hello welcome to my blog">
-            <section>
+            <section className="grid">
                 {data.allMdx.nodes.map((file, idx) => (
                     <article key={idx}>
                         <Link to={`/blog/${file.frontmatter?.slug}`}>
                             <h3>{file.frontmatter?.title}</h3>
                             <h5>{file.frontmatter?.author} in : {file.frontmatter?.category}</h5>
                             <h6>{file.frontmatter?.date}</h6>
-                            <hr />
                         <p>{file.excerpt}</p>
                         </Link>
                     </article>
